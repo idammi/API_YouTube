3c18b5d744bf1f4ccf973b5bc4bca4e52443d9d6
Username <danya221299@gmail.com>
2022-08-17 21:03:27 +0300

    Check exists file

--- work_with_file_system.py
+++ work_with_file_system.py
@@ -1,11 +1,35 @@
+import sys
 from pathlib import Path
+
+
+def file_exists(path_to_file):
+    """Check file exists by path"""
+    path = Path(path_to_file)
+
+    if not path.is_file():
+        raise Exception(f"not found file by {path}")
+
+    return path_to_file
+
+
+def check_path(filename):
+    """
+    This func need for absolut path in the executable file
+    https://stackoverflow.com/questions/22472124/what-is-sys-meipass-in-python
+    """
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        bundle_dir = Path(sys._MEIPASS)
+    else:
+        bundle_dir = Path(__file__).parent
+
+    return file_exists(Path.cwd() / bundle_dir / filename)
 
 
 def get_list_from_file(filename):
     """ generator list from file by path """
 
-    with open(Path(__file__).parent / filename, encoding="utf8", errors='ignore') as file:
-        list_from_file = [line.strip() for line in file.readlines()]
+    with open(check_path(filename), encoding="utf8", errors='ignore') as file:
+        list_from_file = [line.strip() for line in file.readlines() if line is not "\n"]
 
     return list_from_file
 
@@ -16,5 +40,8 @@ def add_info_to_file(filename, info_as_list):
     info_as_list - this foo information which need to write in the file
     """
 
-    with open(Path(__file__).parent / filename, 'a', encoding="utf8", errors='ignore') as file:
-        file.write('\n'.join(str(info_as_str) for info_as_str in info_as_list))
+    with open(check_path(filename), "r+", encoding="utf8", errors="ignore") as file:
+
+        if not file.readlines()[-1] == "\n":
+            file.write("\n")
+        file.write("\n".join(str(info_as_str) for info_as_str in info_as_list))
