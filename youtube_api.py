89fa262c1b913876c6c1163d9ac563ba3ca6bd8d
Username <danya221299@gmail.com>
2022-09-01 15:25:29 +0300

    translate on English(US) select English

--- youtube_api.py
+++ youtube_api.py
@@ -25,50 +25,60 @@ class YouTube(BaseClass):
 
         self.DRIVER.get('http://youtube.com')
 
-        # press button "Войти"
         time.sleep(random.uniform(7, 10))
         self.DRIVER.implicitly_wait(10)
 
-        #  if self.xpath_exists('//tp-yt-paper-button[@aria-label="Accept the use of cookies and other data for the purposes described"]'):
-        if self.xpath_exists(
-                '//tp-yt-paper-button[@aria-label="Accept the use of cookies and other data for the purposes described"]'):
-            self.DRIVER.find_element(By.XPATH,
-                                     value='//tp-yt-paper-button[@aria-label="Accept the use of cookies and other data for the purposes described"]').click()
-            time.sleep(random.uniform(2, 3))
+        # Click button "Accept All" Agreed use all cookies
+        if self.xpath_exists('//tp-yt-paper-dialog'):
 
-        # if self.xpath_exists('//tp-yt-paper-button[@aria-label="Sign in"]'):
-        if self.xpath_exists('//tp-yt-paper-button[@aria-label="Войти"]'):
-            #  self.DRIVER.find_element(By.XPATH, value='//tp-yt-paper-button[@aria-label="Sign in"]').click()
-            self.DRIVER.find_element(By.XPATH, value='//tp-yt-paper-button[@aria-label="Войти"]').click()
+            # check language == English(US)
+            if not self.xpath_exists('//tp-yt-paper-button[@aria-label="English"]'):
 
-            # enter login
-            time.sleep(random.uniform(7, 10))
-            self.DRIVER.implicitly_wait(10)
-            self.DRIVER.find_element(By.XPATH, value='//input[@type="email"]').send_keys(login)
-            time.sleep(random.uniform(.5, 2))
-            self.DRIVER.find_element(By.XPATH, value='//input[@type="email"]').send_keys(Keys.ENTER)
+                # click on the button with lang
+                self.DRIVER.find_element(By.XPATH, value='//tp-yt-paper-button[@class="style-scope ytd-button-renderer style-suggestive size-default"]').click()
 
-            # enter password
-            time.sleep(random.uniform(7, 10))
-            self.DRIVER.implicitly_wait(10)
-            self.DRIVER.find_element(By.XPATH, value='//input[@type="password"]').send_keys(password)
-            time.sleep(random.uniform(.5, 2))
-            self.DRIVER.find_element(By.XPATH, value='//input[@type="password"]').send_keys(Keys.ENTER)
+                # select English as main
+                self.DRIVER.find_element(By.XPATH, value='// yt-formatted-string[text() = "English (US)"]').click()
 
-            # press button "Not now" on question "If you’d like, take a few moments to help Google work better for you"
-            if self.xpath_exists('//button'):
-                self.DRIVER.find_element(By.XPATH, value='//button').click()
+                self.DRIVER.find_elements(By.XPATH,
+                                         value='//tp-yt-paper-button[@aria-disabled="false" and @id="button"]')[5].click()
+                time.sleep(random.uniform(2, 3))
 
-            # if self.xpath_exists('//tp-yt-paper-button[@aria-label="Sign in"]'):
-            if self.xpath_exists('//tp-yt-paper-button[@aria-label="Войти"]'):
-                self.auth()  # This func links on the self
+        # button войти
+        self.DRIVER.find_element(By.XPATH, value='//a[@class="yt-simple-endpoint style-scope ytd-button-renderer"]/tp-yt-paper-button')[1].click()
 
-            return self.welcome_studio()
+        # enter login
+        time.sleep(random.uniform(7, 10))
+        self.DRIVER.implicitly_wait(10)
+        self.DRIVER.find_element(By.XPATH, value='//input[@type="email"]').send_keys(login)
+        time.sleep(random.uniform(.5, 2))
+        self.DRIVER.find_element(By.XPATH, value='//input[@type="email"]').send_keys(Keys.ENTER)
+
+        # enter password
+        time.sleep(random.uniform(7, 10))
+        self.DRIVER.implicitly_wait(10)
+        self.DRIVER.find_element(By.XPATH, value='//input[@type="password"]').send_keys(password)
+        time.sleep(random.uniform(.5, 2))
+        self.DRIVER.find_element(By.XPATH, value='//input[@type="password"]').send_keys(Keys.ENTER)
+
+        # press button "Not now" on question "If you’d like, take a few moments to help Google work better for you"
+        if self.xpath_exists('//button'):
+            self.DRIVER.find_element(By.XPATH, value='//button').click()
+
+        ### Here ADD NEW func on Auth
+
+        # This func links on the self, if not icon account
+        if not self.xpath_exists('//ytd-topbar-menu-button-render/button[@id="avatar-btn]'):
+            self.auth()
+
+
+        return self.prepare_studio()
 
     def create_chanel(self):
         pass
 
-    def welcome_studio(self):
+
+    def prepare_studio(self):
 
         self.DRIVER.get("https://studio.youtube.com/channel/")
         time.sleep(random.uniform(7, 10))
@@ -82,15 +92,15 @@ class YouTube(BaseClass):
             time.sleep(random.uniform(1, 2))
 
         # This table showing after creating new chanel(first enter)
-        if self.xpath_exists('//div[text()="Далее"]'):
-            self.DRIVER.find_element(By.XPATH, value='//div[text()="Далее"]').click()
-            time.sleep(random.uniform(3, 5))
+        # if self.xpath_exists('//div[text()="Далее"]'):
+        #     self.DRIVER.find_element(By.XPATH, value='//div[text()="Далее"]').click()
+        #     time.sleep(random.uniform(3, 5))
 
-    def checker_copyright(self):
+    def founder_issues(self):
 
         # checker copyright(авторское право)
         copyright = self.DRIVER.find_element(By.XPATH, value='//div[@id="results-description"]').text()
-        if copyright == 'Нарушений не найдено.':
+        if copyright == 'No issues found':
             print("Авторское право не нарушено")
 
         elif re.findall(fr'(?im)\bВладелец разрешает\S*\b', copyright) == "Владелец разрешает":
@@ -117,50 +127,43 @@ class YouTube(BaseClass):
         self.DRIVER.find_element(By.XPATH, value='//div[@id="textbox"]').clear()
         self.DRIVER.find_element(By.XPATH, value='//div[@id="textbox"]').send_keys(title)
 
-        # radio-button "for kids"
-        while not self.xpath_exists('//paper-ripple[@checked]'):
+        # check exists the radio-button "for kids"
+        if self.xpath_exists('//tp-yt-paper-radio-button[@name="VIDEO_MADE_FOR_KIDS_MFK"]'):
 
-            # check exists the radio-button "for kids"
-            if self.xpath_exists('//tp-yt-paper-radio-button[@name="VIDEO_MADE_FOR_KIDS_MFK"]'):
+            # click on the radio-button "for kids"
+            self.DRIVER.find_element('//div[@class="style-scope tp-yt-paper-radio-button"]').click()
 
-                # click on the radio-button "for kids"
-                self.DRIVER.find_elements('class="style-scope tp-yt-paper-radio-button"')[0].click()
 
-            else:
-                print('xpath radio "for kids" don\'t work')
-        else:
-            print('radio "for kids" turns on')
+        # open new param, pressed button "Show more"
+        self.DRIVER.find_element(By.XPATH, value='//div[text()="Show more"]').click()
 
-            # open new param, pressed button "Развернуть"
-            self.DRIVER.find_element(By.XPATH, value='//div[text()="Развернуть"]').click()
-
-            # add tags
-            time.sleep(random.uniform(5, 8))
-            self.DRIVER.implicitly_wait(10)
-            self.DRIVER.find_element(By.XPATH, value='//input[@aria-label="Теги"]').clear()
-            self.DRIVER.find_element(By.XPATH, value='//input[@aria-label="Теги"]').send_keys('#shorts' + str(tags))
+        # add tags
+        time.sleep(random.uniform(5, 8))
+        self.DRIVER.implicitly_wait(10)
+        self.DRIVER.find_element(By.XPATH, value='//input[@aria-label="Tags"]').clear()
+        self.DRIVER.find_element(By.XPATH, value='//input[@aria-label="Tags"]').send_keys('#shorts' + str(tags))
 
         # press button "Next"
-        if self.xpath_exists('//div[text()="Далее"]'):
+        if self.xpath_exists('//div[text()="Next"]'):
 
             # from page "information" to "Adds"
-            self.DRIVER.find_elements('//div[text()="Далее"]')[1].click()
+            self.DRIVER.find_elements('//div[text()="Next"]')[1].click()
 
     def page3_upload_video(self):
         # from page "Adds" to "Checker YouTube"
         time.sleep(random.uniform(2, 4))
         self.DRIVER.implicitly_wait(10)
-        self.DRIVER.find_elements('//div[text()="Далее"]')[1].click()
+        self.DRIVER.find_elements('//div[text()="Next"]')[1].click()
 
     def page4_upload_video(self):
 
         # check uploaded video on the copyright and baneded content
-        self.checker_copyright()
+        self.founder_issues()
 
         # from page "Checker YouTube" to access
         time.sleep(random.uniform(2, 4))
         self.DRIVER.implicitly_wait(10)
-        self.DRIVER.find_elements('//div[text()="Далее"]')[1].click()
+        self.DRIVER.find_elements('//div[text()="Next"]')[1].click()
 
     def page5_upload_video(self):
         # select radio-button public access
@@ -173,7 +176,7 @@ class YouTube(BaseClass):
             status_now = self.DRIVER.find_element(
                 '//span[@class="progress-label style-scope ytcp-video-upload-progress"]').text()
             print(status_now)
-            if not status_now.split(".")[0] == "Проверка завершена":
+            if not status_now.split(".")[0] == "Checks complete":
                 status()
 
         status()
@@ -204,7 +207,7 @@ class YouTube(BaseClass):
         # press button "Close"
         time.sleep(random.uniform(1, 4))
         self.DRIVER.implicitly_wait(10)
-        self.DRIVER.find_element('//div[text()="Закрыть"]').click()
+        self.DRIVER.find_element('//div[text()="Close"]').click()
 
         time.sleep(20)
 
