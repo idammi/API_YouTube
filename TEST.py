a8e3385e80aca8ddd2df81f5e6926a520f72a6c9
ArtDanger <84302568+ArtDanger@users.noreply.github.com>
2022-09-03 10:11:38 +0000

    Delete TEST.py

--- TEST.py
+++
@@ -1,9 +0,0 @@
-import selenium_driver
-
-base = selenium_driver.BaseClass()
-DRIVER = base.driver()
-
-DRIVER.get('https://accounts.google.com/signin/chrome/sync/identifier?ssp=1&continue=https%3A%2F%2Fwww.google.com%2F&flowName=GlifDesktopChromeSync')
-
-DRIVER.close()
-DRIVER.quite()
